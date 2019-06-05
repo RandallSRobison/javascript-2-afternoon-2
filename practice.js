c292ee3d3f8be24d11f72c645ef03e3d20b605ed
Spence Robison <spence@Spences-MacBook-Pro.local>
2019-06-05 16:05:41 -0600

    finished the top section

--- practice.js
+++ practice.js
@@ -53,7 +53,8 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 function looper(family){
-  
+  for (var i = 0; i < family.length; i++)
+  window.alert(family[i])
 }
 
 
@@ -69,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for( let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -84,7 +89,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  evenArray = []
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 === 0){
+      evenArray.push(nums[i])
 
+    }
+  }
+  return evenArray
+}
 
 
 
